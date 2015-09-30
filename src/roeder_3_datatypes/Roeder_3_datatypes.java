ea0606a53f8a620d313c91bb33908e365cc81bcb
Anthony Roeder <aroeder7135@stu.neisd.net>
2015-09-30 06:12:36 -0500

    DataTypes

---
+++ src/roeder_3_datatypes/Roeder_3_datatypes.java
@@ -0,0 +1,76 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package roeder_3_datatypes;
+
+/**
+ *
+ * @author aroeder
+ */
+public class Roeder_3_datatypes {
+
+     public static void main(String[] args) {
+     //This is a comment
+     //We will use many comments to explain
+     //our code
+     //This program explores some of the 
+     //primitive data types
+         int i = 1;
+         float a = 2.5f;
+         //f tells it to recognize that it is a float
+         double b = 3.87;
+         short c = 16;
+         char d1 = ';';
+         char d2 = 'a';
+         char d3 = 42;
+         char d4 = 145;
+         String e = d4+"ninja";
+         String e1 = "Anthony Roeder Is The Bestest Football Player EVER";
+         boolean f = true;
+         boolean g = 1 % 2 == 0;
+         //string needs to be capitalized "String"
+         
+         /*
+         Here we will look at some mathmatical operators
+         */
+       // + this adds two numbers and more
+       // - this subtracts two numbers and more
+       // * This multiplies two numbers and more
+       // / This divides two numbers and more
+       // % this is modulo. Shows the remainder
+       
+       System.out.println(i+ " an integer");
+       System.out.println(a+ " a float");
+       System.out.println(b+ " a double");
+       System.out.println(c+ " a short");
+       System.out.println(d1+ " a char");
+       System.out.println(d2+ " a char");
+       System.out.println(d3+ " a char");
+       System.out.println(d4+ " a char");
+       System.out.println(e + " a string");
+       System.out.println(e+ " a string");
+       System.out.println(f+ " a boolean");
+       System.out.println(g+ " a boolean");
+       System.out.println(5 + " a boolean");
+//       for(i =- -123;i < 123; i++) {
+//           //System.out.println((char) 1);
+//           System.out.println(i % 3 ==0);
+     //}
+       // a number value for char
+       // maps to a character
+       i = 7;
+       a = 0;
+       e = "ninja";
+       System.out.println(i+ a + " an integer/" + "plus a float ");
+       System.out.println(e + i + " an /" + "a string plus an integer");
+       System.out.println(a + " a float/" + "plus a double");
+       System.out.println(b + " a double/" + "plus a short");
+       System.out.println(c + " a short");
+     }
+}
+       
+    
+    
+
